1c922741a7903003fdbb5fa54c2058be33b339e3
bagaking <zfaustk@qq.com>
2018-09-17 00:42:08 +0800

    feat (web3/contracts): create contracts - CERC20, CERC721

--- src/web3/contracts/cerc721.js
+++ src/web3/contracts/cerc721.js
@@ -2,25 +2,12 @@ import {Contractance} from "../contractance";
 
 /**
  * contract which implement interfaces of erc20
- * @see https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+ * @see https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
  * @extends Contractance
  * @desc
  * the interface of contracts
- * ```solidity
- *  contract ERC20Interface {
- *       function totalSupply() public view returns (uint256);
- *       function balanceOf(address tokenOwner) public view returns (uint256 balance);
- *       function allowance(address tokenOwner, address spender) public view returns (uint256 remaining);
- *       function transfer(address to, uint256 tokens) public returns (bool success);
- *       function approve(address spender, uint256 tokens) public returns (bool success);
- *       function transferFrom(address from, address to, uint256 tokens) public returns (bool success);
- *
- *       event Transfer(address indexed from, address indexed to, uint256 tokens);
- *       event Approval(address indexed tokenOwner, address indexed spender, uint256 tokens);
- *  }
- *  ```
  */
-export default class CERC20 extends Contractance {
+export default class CERC721 extends Contractance {
 
     constructor(pInstance, abi) {
         super(pInstance, abi)
---
+++ src/web3/contracts/index.js
@@ -0,0 +1,2 @@
+export CERC721 from './cerc721'
+export CERC20 from './cerc20'
