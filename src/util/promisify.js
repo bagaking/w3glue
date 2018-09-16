94fbf7dd2147b15d75b26d18f8ef639c42848446
bagaking <zfaustk@qq.com>
2018-09-16 23:45:18 +0800

    feat (util): add promisify

---
+++ src/util/promisify.js
@@ -0,0 +1,43 @@
+let Promisify = (fn, receiver = null) => {
+    return (...args) => {
+        return new Promise((resolve, reject) => {
+            let cb = (err, res) => {
+                return err ? reject(err) : resolve(res)
+            }
+            if(receiver === null){
+                fn(...args, cb)
+            } else {
+                fn.apply(receiver, ...args, cb)
+            }
+        })
+    }
+}
+
+/**
+ * Call a function, with no receiver, and return the promise
+ * @param {function} fn - the last argument of this function should be callback
+ * @param {...any} args - arguments
+ * @returns {Promise<void>}
+ */
+let PromiseFunctionCall = function(fn, ...args) {
+    return Promisify(fn)(...args)
+}
+
+/**
+ * Call a method, with a receiver, and return the promise
+ * @param {function} fn -  the last argument of this function should be callback
+ * @param {Object} receiver - the receiver(`this`) of this function
+ * @param {...any} args - arguments
+ * @returns {Promise<void>}
+ */
+let PromiseMethodCall = function(fn, receiver, ...args) {
+    return Promisify(fn, receiver)(...args)
+}
+
+module.exports = {
+    Promisify,
+
+    PromiseFunctionCall,
+
+    PromiseMethodCall
+}
