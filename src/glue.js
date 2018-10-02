277abe20912404c841e44e000f82a36d82830fd3
bagaking <zfaustk@qq.com>
2018-10-02 18:01:18 +0800

    fix: exports of Contract in glue.js

--- src/glue.js
+++ src/glue.js
@@ -4,6 +4,6 @@ module.exports = {
     "util" : require("./util"),
     "web3" : require("./web3"),
     "w3" : require("./w3"),
-    CBoard : require("./core/board")
-    Contract : require("../core/contract")
+    CBoard : require("./core/board"),
+    Contract : require("./core/contract")
 }
