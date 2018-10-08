c3671cb4b54b81dd012451e7084ddbc1a4ad9f77
bagaking <zfaustk@qq.com>
2018-10-08 16:10:17 +0800

    build: export Network

--- src/glue.js
+++ src/glue.js
@@ -4,7 +4,10 @@ module.exports = {
     "util" : require("./util"),
     "web3" : require("./web3"),
     "w3" : require("./w3"),
+
+    Network: require("./core/network").Network,
     Mux: require("./core/mux"),
     CBoard : require("./core/board"),
     Contract : require("./core/contract")
+
 }
