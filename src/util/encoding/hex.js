1886cae9c2ca61d2f29a2115d616a11d8bbd7997
bagaking <zfaustk@qq.com>
2018-10-11 01:20:53 +0800

    feat (util): re-implement rlp

---
+++ src/util/encoding/hex.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const _ = require('lodash')
+
+/**
+ * pad to even hex string
+ * @param {string|number} v
+ * @return {string}
+ */
+const numOrStrToEvenHex = (v) => v.toString(16).padStart(2, '0');
+
+const isHexStr = hex => _.isString(hex) && /^(-0x|0x)?[0-9a-f]*$/i.test(hex);
+
+const utf8StrToHex = utf8Str => // before encoding, 0 at start or end should be removed
+    _.map(utf8Str.replace(/^(?:\u0000)*/g, '').replace(/(?:\u0000)*$/g, ''),
+        (s, i, o) => o.charCodeAt(i).toString(16).padStart(2))
+
+
+function toHex(value) {
+    let ret = ''
+
+    if (_.isBoolean(value))
+        return toHex(value ? 1 : 0);
+
+    if (_.isNumber(value))
+        return numOrStrToEvenHex(value);
+
+    if (_.isString(value)) {
+        if (isHexStr(value)) {
+            throw new Error("implement me ! ")//todo
+        } else {
+            return numOrStrToEvenHex(value);
+        }
+    }
+
+    if (_.isObject(value)) {
+        return utf8StrToHex(JSON.stringify(value))
+    }
+
+    return ret
+}
+
+module.exports = {
+    numOrStrToEvenHex
+}
+
+
---
+++ src/util/encoding/rlp.js
@@ -0,0 +1,109 @@
+'use strict'
+
+/**
+ * @fileOverview
+ *
+ * RLP Encoding
+ *
+ * @see {@link https://github.com/ethereum/wiki/wiki/RLP}
+ *
+ * formation:
+ *
+ * | type                       | head                          | payload               | memo                              |
+ * | ----                       | --------                      | -------               | ----                              |
+ * | single char [0, 0x7f]      | <empty>                       | [0, 0x7f]             |                                   |
+ * | string (length < 56)       | [0x80, 0xb7] (56)             | string                |                                   |
+ * | string (length >= 56)      | [0xb7, 0xbf] (8) .. length    | string                | max length can be 16 ** 8 == 4T   |
+ * | array (length < 56)        | [0xc0, 0xf7] (56)             | string                |                                   |
+ * | array (length >= 56)       | [0xf7, 0xff] (8) .. length    | string                | max length can be 16 ** 8 == 4T   |
+ *
+ */
+
+const _ = require("lodash")
+const {Buffer} = require('safe-buffer')
+const {numOrStrToEvenHex} = require('./hex')
+
+/**
+ * encode a item that is 'A string (ie. byte array)'
+ * @param {Buffer|Uint8Array|string} buf
+ * @private
+ */
+function _encodeBuf(buf) {
+    buf = Buffer.from(buf)
+    return buf.length === 1 && buf[0] < 128 ? input : _appendHead(0x80, buf)
+}
+
+/**
+ * encode a item that is 'A list of items'
+ * @param {Array} arr
+ * @private
+ */
+function _encodeArr(arr) {
+    return _appendHead(0xc0, Buffer.concat(_.map(arr, _encodeBuf)))
+}
+
+/**
+ * append the control word of the buffer
+ *
+ * @desc
+ *
+ * a control word should follow rules of:
+ *
+ * | type                       | head
+ * | ----                       | --------
+ * | single char [0, 0x7f]      | <empty>
+ * | string (length < 56)       | [0x80, 0xb7] (56)
+ * | string (length >= 56)      | [0xb7, 0xbf] (8) .. length
+ * | array (length < 56)        | [0xc0, 0xf7] (56)
+ * | array (length >= 56)       | [0xf7, 0xff] (8) .. length
+ *
+ * @param {Buffer} buf
+ * @param {number} typeOffset - for array it is 0xc0(192), and for raw string it is 0x80(128)
+ * @return {Buffer}
+ */
+function _appendHead(typeOffset, buf){
+    let lenBuf = buf.length
+    if (lenBuf < 56) {
+        // Concat with Buffer is about two times fast than Uint8Array
+        return Buffer.concat([Buffer.from([typeOffset + lenBuf]), buf])
+    } else if (lenBuf < Number.MAX_SAFE_INTEGER) {
+        // there are up to 8 Byte using to present the length, thus the max length of data is 2 ** (8 * 8)
+        // but in js, the largest accurate number is Number.MAX_SAFE_INTEGER
+        let hexStrForLen = numOrStrToEvenHex(lenBuf)
+        let head = numOrStrToEvenHex(typeOffset + 55 + (hexStrForLen.length / 2))
+        return Buffer.concat([Buffer.from(head + hexStrForLen, 'hex'), buf])
+    } else {
+        throw new Error(`content size ${lenBuf} exceeded`)
+    }
+}
+
+/**
+ * encode an item
+ * @param {Array|Buffer|Uint8Array|string} item
+ * @return {Buffer}
+ */
+function encode(item) {
+    if(_.isArray(item)) {
+        return _encodeArr(item)
+    } else if(_.isBuffer(item) || _.isString(item) || item instanceof Uint8Array) {
+        return _encodeBuf(item)
+    } else{
+        throw new Error("type error: item must be a string (ie. byte array)")
+    }
+}
+
+/**
+ * decode an buffer
+ * @param {Buffer} buf - encoded buffer
+ */
+function decode(buf) {
+    if(!_.isBuffer(buf)) {
+        throw new Error("type error: the input of decode must be a buffer")
+    }
+
+}
+
+module.exports = {
+    encode,
+    decode
+}
